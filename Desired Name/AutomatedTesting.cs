8d758b3bcc90969469b51510dd8744a20de014a4
raihantitas <aburaihan@iut-dhaka.edu>
2020-12-28 23:48:02 +0600

    Automated test Done
    Tested correctly

---
+++ Desired Name/AutomatedTesting.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desired_Name
+{
+    class AutomatedTesting
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://elp.duetbd.org/login/index.php";
+            IWebElement element1 = driver.FindElement(By.XPath("//*[@id='username']"));
+            element1.SendKeys("170042026");
+            IWebElement element2 = driver.FindElement(By.XPath("//*[@id='password']"));
+            element2.SendKeys("********");
+            IWebElement element3 = driver.FindElement(By.XPath("//*[@id='loginbtn']"));
+            element3.Click();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
